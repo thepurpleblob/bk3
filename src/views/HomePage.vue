a4337765e3ecde6546fca9a014a6cde24b289eeb
Howard Miller <howardsmiller@gmail.com>
2025-06-16 14:09:06 +0100

    adding video on home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -19,13 +19,19 @@
             imageleft="true"
             :list="fareslist"
         ></LayoutBlock>
+
+        <div v-if="page.video" class="row my-4 py-1 border border-3 rounded-2">
+            <div v-html="page.video"></div>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 import LayoutBlock from '@/components/LayoutBlock.vue';
 
+const url = process.env.VUE_APP_ENDPOINT;
 const fareslist = ref([
     {
         n: 1,
@@ -46,8 +52,23 @@ const fareslist = ref([
         routerlink: '/plan',
     }
 ]);
+const page = ref({});
+
+onMounted(() => {
+    axios.get(url + '/homepage')
+    .then(result => {
+        page.value = result.data.data;
+    })
+    .catch(error => {
+        window.console.error(error);
+    });
+});
 </script>
 
 <style>
-
+    iframe {
+        aspect-ratio: 16 / 9;
+        width: 100% !important;
+        height: auto !important;
+    }
 </style>
